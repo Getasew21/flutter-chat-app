31acafd2c82053f8d7002a13834145576c11c424
Getasew21 <getasewadane9@gmail.com>
2023-03-04 21:00:19 +0300

    firebase cofigered and installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:chat_app/screens/ChatScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp( const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(8),
-                child: Text("this works!"),
-              )),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chats/2vDKciFOiNJVfH63WdQK/messages")
+            .snapshots(),
+        builder: (ctx, snapshot) {
+          final documents = snapshot.data?.docs;
+          return ListView.builder(
+              itemCount: documents?.length,
+              itemBuilder: (ctx, index) => Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(documents?[index]["text"]),
+                  ));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {
+          FirebaseFirestore.instance
+              .collection("chats/2vDKciFOiNJVfH63WdQK/messages")
+              .add({"text": "this was added by me"});
+        }),
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
