5929e6207a2335d08a22556b29adb763c65e20fc
Getasew21 <getasewadane9@gmail.com>
2023-03-04 20:59:14 +0300

    firebase is configerd and installed to the app

--- lib/screens/AuthScreen.dart
+++ lib/screens/AuthScreen.dart
@@ -1,8 +1,13 @@
 import "package:flutter/material.dart";
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
